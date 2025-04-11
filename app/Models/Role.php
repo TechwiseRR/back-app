d7d8b356c4358a9d4be8fdaad23da598ec309cd5
N-Dinot <neo.dinot@gmail.com>
2025-04-11 14:45:27 +0200

    feat : role model add

---
+++ app/Models/Role.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'roleName',
+        'rank',
+        'permission_id',
+    ];
+
+    /**
+     * Relation avec la permission.
+     * Un rôle possède une permission.
+     */
+    public function perm()
+    {
+        return $this->belongsTo(Permission::class, 'permission_id');
+    }
+
+    // ==== Champs privés ====
+
+    /**
+     * @var int
+     */
+    private int $id;
+
+    /**
+     * @var string
+     */
+    private string $roleName;
+
+    /**
+     * @var int
+     */
+    private int $rank;
+
+    /**
+     * @var int
+     */
+    private int $permission_id;
+}
