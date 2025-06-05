c9e8b1f7b294238415cf4e98bd80e1dc4183e2db
maber <marjane.aberkan@viacesi.fr>
2025-06-05 16:15:12 +0200

    Mise a jour du model User Ajout du controller User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    //
+    public function user()
+    {
+        $user = auth()->user();
+
+        if (!$user) {
+            return response()->json(['error' => 'Utilisateur non authentifié'], 401);
+        }
+
+        return response()->json(auth()->user()->makeHidden(['password']));
+    }
 }
